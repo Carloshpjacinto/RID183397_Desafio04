8586e0d431c5ef431192629eab05db6352050eaa
Carloshpjacinto <carlo.henrique37@gmail.com>
2024-11-07 15:49:01 -0300

    feature/media-queries

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 import { LogoFigma, LogoGitHub, LogoLinkedin } from "@/assets";
+import { TamanhoResponsivo } from "@/utils";
 
 const StyledFooter = styled.footer`
 
@@ -23,6 +24,36 @@ const StyledFooter = styled.footer`
         margin-left: 5rem;
         cursor: pointer;
     }
+
+    @media ${TamanhoResponsivo.laptop}{
+
+        margin-bottom: 5rem;
+
+        .contato{
+
+            font-size: 2.5rem;
+            margin: -5rem;
+        }
+
+        .contato:last-child{
+
+            margin-left: 10rem;
+        }
+
+        .LogoFooter{
+
+            width: 5rem;
+            height: 5rem;
+            margin-left: 3rem;
+            cursor: pointer;
+        }
+
+        .posicaoLink{
+
+            margin-right: -5rem;
+        }
+    
+    }
 `
 
 function Footer(){
@@ -35,7 +66,7 @@ function Footer(){
 
                 <h3 className="contato">Email:<br/>carlo.henrique37@gmail.com</h3>
             </div>
-            <div>
+            <div className="posicaoLink">
                 <a href="https://github.com/Carloshpjacinto" target="_blank"><img className="LogoFooter" src={LogoGitHub} alt="Logo GitHub" /></a>
 
                 <a href="https://www.linkedin.com/in/carlos-henrique-parreira-jacinto-1962b0179/" target="_blank"><img className="LogoFooter" src={LogoLinkedin} alt="Logo Linkedin" /></a>
